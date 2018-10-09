d4037d110429b93e9d84ec86ec9d692d22c2a99c
oomusou <oomusou@gmail.com>
2018-10-09 14:40:51 +0800

    Simple Counter

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,14 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': 'off',
+    'arrow-parens': 'off',
+    'no-return-assign': 'off',
+    'no-shadow': 'off',
+    'func-names': 'off',
+    'import/prefer-default-export': 'off',
+    'space-before-function-paren': 'off',
+    'no-plusplus': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
